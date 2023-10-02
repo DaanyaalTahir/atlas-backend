561235c12dccc45f238d8d1ae6d37a6584897f54
Jp <jeanpaul.saliba@ontariotechu.net>
2023-10-01 23:09:46 -0400

    Working on Deployment

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+require("dotenv").config(); // Load environment variables from .env
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3306;
+
+// Configure MySQL connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "fifastreet12",
+  database: "atlasdb",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to MySQL database");
+});
+
+app.use(bodyParser.json());
+
+// Define API endpoints for CRUD operations
+app.get("/api/data", (req, res) => {
+  db.query("SELECT * FROM userinfo", (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+// Add more endpoints for CRUD operations (POST, PUT, DELETE)...
 
 app.get("/", (req, res) => {
   res.send({ message: "Welcome to the beginning of Project Atlas =)" });
