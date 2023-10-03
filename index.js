8da181d3694d03ff1a4e335c37c813dd1bc6db74
Jp <jeanpaul.saliba@ontariotechu.net>
2023-10-03 15:41:01 -0400

    add changes new

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 33570;
 
 const connection = mysql.createConnection({
-  host: "s465z7sj4pwhp7fn.cbetxkdyhwsb.us-",
+  host: "s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   user: " os0hrbkfsr8bdiv",
   password: "c57gyhp7h3ml6iv0",
   database: "je0zyzny9yrih8nc",
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 // Define API endpoints for CRUD operations
 app.get("/api/data", (req, res) => {
-  connection.query("SELECT * FROM userinfo", (err, results) => {
+  connection.query("SELECT * FROM users", (err, results) => {
     if (err) throw err;
     res.json(results);
   });
